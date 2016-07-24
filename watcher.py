32dc929d0986bfcf86ff3971792ef3713f2196e2
Frinter <codewad@gmail.com>
2016-07-24 13:47:37 -0700

    Add some more default parameters and things

--- watcher.py
+++ watcher.py
@@ -2,11 +2,16 @@ import os
 import sys
 import time
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from watchdog.events import PatternMatchingEventHandler
 from debounce import debounce
 
-class FSCommandRunningEventHandler(FileSystemEventHandler):
-    def __init__(self, command):
+class CommandRunningEventHandler(PatternMatchingEventHandler):
+    def __init__(self, command, watchPatterns, ignorePatterns):
+        super(CommandRunningEventHandler, self).__init__(
+            patterns=watchPatterns,
+            ignore_patterns=ignorePatterns,
+            ignore_directories=True
+        )
         self.command = command
 
     def on_any_event(self, event):
@@ -18,37 +23,55 @@ def getRunCommand(command):
         os.system(command)
     return internalRun
 
-
 if __name__ == '__main__':
-    parameterMap = {
-        'd': 'path'
-    }
+    def getParameters(parameterMap, defaults, arguments):
+        def parseArgs(args):
+            index = 0
+            parameters = {}
+            while args[index][0] == '-':
+                parameters[parameterMap[args[index][1]]] = args[index+1]
+                index += 2
 
-    def parseArgs(args):
-        index = 0
-        parameters = {}
-        while args[index][0] == '-':
-            parameters[parameterMap[args[index][1]]] = args[index+1]
-            index += 2
+            parameters['rest'] = ' '.join(args[index:])
+            return parameters
 
-        parameters['command'] = ' '.join(args[index:])
+        suppliedParameters = parseArgs(arguments)
+        parameters = {
+            'rest': suppliedParameters['rest']
+        }
+
+        for (key, value) in defaultParameters.items():
+            parameters[key] = suppliedParameters[key] if key in suppliedParameters else defaultParameters[key]
+
         return parameters
 
-    parameters = parseArgs(sys.argv[1:])
-    path = parameters['path'] if 'path' in parameters else '.'
-    command = getRunCommand(parameters['command'])
-    doCommandSoon = debounce(command, 0.01)
+    parameterMap = {
+        'd': 'path',
+        'w': 'watch',
+        'i': 'ignore'
+    }
 
-    print "Watching path ", path
-    eventHandler = FSCommandRunningEventHandler(doCommandSoon)
+    defaultParameters = {
+        'path': '.',
+        'watch': '*',
+        'ignore': [os.path.join('.', '.git', '*'), os.path.join('.', '.#*'), os.path.join('.', '*', '.#*')]
+    }
+
+    parameters = getParameters(parameterMap, defaultParameters, sys.argv[1:])
+    command = getRunCommand(parameters['rest'])
+    doCommandSoon = debounce(command, 0.05)
+
+    print "Watching path ", parameters['path'], ", Ignoring patterns", parameters['ignore']
+    print "Will run command \"" + parameters['rest'] + "\""
+    eventHandler = CommandRunningEventHandler(doCommandSoon, parameters['watch'], parameters['ignore'])
 
     observer = Observer()
-    observer.schedule(eventHandler, path, recursive=True)
+    observer.schedule(eventHandler, parameters['path'], recursive=True)
     observer.start()
 
     try:
         while True:
-            time.sleep(0.1)
+            time.sleep(0.2)
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
